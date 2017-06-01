a51f86e8618915eaa21fbbd765653847036eedf4
Pierre DUBAILLAY <pierredubaillay@orange.fr>
2017-06-01 10:14:55 +0200

    Add PluginLoader class for easy plugin load
    DummyPlugin has been updated for test purpose. Version number increased.

--- extra/dummyPlugin.py
+++ extra/dummyPlugin.py
@@ -1,7 +1,41 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 
-__version__ = '1.0'
+import src.core.model
+import PyQt5.QtWidgets as QtWidgets
+import PyQt5.QtCore as QtCore
 
-class DummyPlugin(object):
-    pass
+__version__ = '1.1'
+
+
+class DummyPlugin(QtWidgets.QWidget):
+    """
+    A dummy plugin, with nothing to show except a nice label.
+    """
+
+    def __init__(self, parent=None):
+        super().__init__(parent=parent, flags=QtCore.Qt.Widget)
+        self._create_view()
+        self._place_components()
+
+    def _create_view(self):
+        """
+        Create the poor label alone.
+        """
+        self._poor_label = QtWidgets.QLabel(text='Poor label here', parent=self)
+
+    def _place_components(self):
+        """
+        Place the poor label in the view.
+        """
+        layout = QtWidgets.QGridLayout(self)
+        layout.addWidget(self._poor_label, 1, 1)
+
+
+loader = src.core.model.PluginLoader(
+    name='DummyPlugin',
+    version=__version__,
+    info='It\'s just a DummyPlugin, nothing to say !',
+    authors=('p76dub'),
+    plugin=DummyPlugin(),
+)
--- src/core/model.py
+++ src/core/model.py
@@ -284,3 +284,69 @@ class TwentyThreeToolsMenuBarModel(QtCore.QObject):
         action.triggered.connect(lambda e: self.plugin_selected.emit(plugin))
 
         return action
+
+
+class PluginLoader():
+    """
+    Class that every plugin needs to instantiate. This instance should be available in the __init__
+    file of the plugin's package.
+    """
+
+    def __init__(self, **kwargs):
+        """
+        Create a new instance of PluginLoader.
+        :param kwargs: dictionary that can contain this pairs:
+            - name -> str
+            - version -> str
+            - info -> str
+            - authors -> tuple<str>
+            - plugin -> PyQt5.QtWidgets.QWidget
+        """
+        self._attributes = {
+            'name' : str(hash(self)),
+            'version' : 'unknown',
+            'info' : 'No information',
+            'authors' : (),
+            'plugin' : QtWidgets.QWidget(flags=QtCore.Qt.Widget),
+        }
+
+        self._attributes.update(kwargs)
+
+    @property
+    def plugin_name(self):
+        """
+        Get the plugin's name.
+        :return: a name (str)
+        """
+        return str(self._attributes['name'])
+
+    @property
+    def plugin_version(self):
+        """
+        Get the plugin's version.
+        :return: a version number (str)
+        """
+        return str(self._attributes['version'])
+
+    @property
+    def plugin_info(self):
+        """
+        Get some information about the plugin.
+        :return: a description of the plugin (str)
+        """
+        return str(self._attributes['info'])
+
+    @property
+    def plugin_authors(self):
+        """
+        Get plugin's author(s).
+        :return: a tuple of authors (tuple<str>)
+        """
+        return tuple(map(str, self._attributes['authors']))
+
+    def load(self):
+        """
+        Load the plugin.
+        :return: an instance of PyQt5.QtWidgets.QWidget
+        """
+        return self._attributes['plugin']
