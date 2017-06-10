e64850ebc86e26315e11d7e01aef5fba84023634
Pierre DUBAILLAY <pierredubaillay@orange.fr>
2017-06-10 19:48:51 +0200

    Scandable plugin version updated
    Controllers are working, new options to filter the result.
    
    Minor changes in the main application layout.

--- extra/scandable.py
+++ extra/scandable.py
@@ -2,12 +2,15 @@
 """
 This module contains what necessary for the Scandable plugin.
 """
+import string
+
 import PyQt5.QtCore as QtCore
 import PyQt5.QtWidgets as QtWidgets
 
 import src.core.utils
+import src.core.widgets.dialog
 
-__version__ = '1.0a2'
+__version__ = '1.0'
 
 
 class ScandableModel(QtCore.QObject):
@@ -18,8 +21,10 @@ class ScandableModel(QtCore.QObject):
         get_separators() is not None
         len(get_separators()) >= 1
     """
+    # Shift we need to perform to have the order in the alphabet
+    _SHIFT = ord('a') - 1
 
-    def __init__(self, number, file=None, separators=frozenset(' ')):
+    def __init__(self, number, file=None, separators=frozenset(string.whitespace)):
         """
         Constructor.
         :param number: packet's number
@@ -30,17 +35,22 @@ class ScandableModel(QtCore.QObject):
             number >= 1
         :post: get_separators() == separators
             get_file == file
+            get_options() == {'no-repeat':False, 'no-blank':True}
         """
         super().__init__()
         if separators is None or len(separators) < 1:
-            raise AssertionError()
+            raise AssertionError('No separators provided')
         if number < 1:
-            raise AssertionError()
+            raise AssertionError('Positive number expected')
 
-        self._num = number
+        self._num = int(number)
         self._file = file
         self._offset = 0
         self._separators = set(separators)
+        self._options = {
+            'no-blank' : True,
+            'no-repeat' : False,
+        }
 
     def get_file(self):
         """
@@ -56,6 +66,15 @@ class ScandableModel(QtCore.QObject):
         """
         return self._separators
 
+    def get_options(self):
+        """
+        Retrieve options associated with the model.
+        :return: a dictionary containing the following keys:
+            - no-blank : if True, no blanks in the result
+            - no-repeat : if True, words in the result are unique
+        """
+        return dict(self._options)
+
     def set_file(self, file=None):
         """
         Change the file that should be processed. Default is None
@@ -63,6 +82,16 @@ class ScandableModel(QtCore.QObject):
         """
         self._file = file
         self._offset = 0
+
+    def set_options(self, options):
+        """
+        Set options associated with the model.
+        Authorized keys are :
+            - no-repeat : words in the result are unique
+            - no-blank : no empty strings in the result
+        :param options: the dictionary
+        """
+        self._options.update(options)
 
     def set_separators(self, separators):
         """
@@ -73,7 +102,7 @@ class ScandableModel(QtCore.QObject):
         :post: get_separators() == separators
         """
         if separators is None or len(separators) < 1:
-            raise AssertionError()
+            raise AssertionError('No separators provided')
 
         self._separators = separators
 
@@ -86,7 +115,7 @@ class ScandableModel(QtCore.QObject):
         :throws: IOError if an error occurs
         """
         if self.get_file() is None:
-            raise AssertionError()
+            raise AssertionError('No file provided')
 
         # Reset shift at 0
         self._offset = 0
@@ -97,7 +126,7 @@ class ScandableModel(QtCore.QObject):
             for line in file_descriptor:
                 result.extend(self._parse(line.strip(), sep))
 
-        return result
+        return self._format(result)
 
     def parse_line(self, sep=None):
         """
@@ -109,14 +138,14 @@ class ScandableModel(QtCore.QObject):
         :throws: IOError if an error occurs
         """
         if self.get_file() is None:
-            raise AssertionError()
+            raise AssertionError('No file provided')
 
         with open(self._file, 'r') as file_descriptor:
             file_descriptor.seek(self._offset)
             line = file_descriptor.readline()
             self._offset = file_descriptor.tell()
 
-        return self._parse(line.strip(), sep)
+        return self._format(self._parse(line.strip(), sep))
 
     def _parse(self, sentence, sep=None):
         """
@@ -130,29 +159,53 @@ class ScandableModel(QtCore.QObject):
         if sep is not None:
             separators = str.join('', sep)
 
-        # Shift we need to perform to have the order in the alphabet
-        shift = ord('a') - 1
-
-        # Slice words according to separators
+        # Slice words according to separators and analyse
         sentence_lower = sentence.lower()
-        words = sentence_lower.split(separators)
-
-        # Analyse
         result = []
-        for word in words:
-            value = 0
-            i = 0
-            recognized = True
-            while i < len(word) and recognized:
-                value += ord(word[i]) - shift  # number in the alphabet
-                if value > self._num:
-                    recognized = False  # un paquet ne fait pas 23
-                elif value == self._num:
-                    value = 0
-                i += 1
+        start = 0
+        for end in range(len(sentence_lower)):
+            word = None
+            # Slicing
+            if sentence_lower[end] in separators:
+                word = sentence_lower[start:end]
+                start = end + 1
+            elif  end == len(sentence_lower) - 1:
+                word = sentence_lower[start:end + 1]
 
-            if recognized and value == 0:  # verify if last packet has the right size
+            # Run analysis
+            if word is not None and self._analyse_word(word):
                 result.append(word)
+
+        return result
+
+    def _analyse_word(self, word):
+        """
+        Analyse word and return True if it is scandable, else False
+        :param word: the word that will be analyzed
+        :return: True our False
+        """
+        value = 0
+        i = 0
+        recognized = True
+        while i < len(word) and recognized:
+            value += ord(word[i]) - self._SHIFT  # number in the alphabet
+            if value > self._num:
+                recognized = False  # One packet is not self._num
+            elif value == self._num:
+                value = 0
+            i += 1
+        return recognized and value == 0
+
+    def _format(self, result):
+        """
+        Format a list of strings according to options. Careful, result is formatted in place !
+        :param result: the list
+        :return: the formatted result
+        """
+        if self._options['no-blank']:
+            result = [r for r in result if r != '']
+        if self._options['no-repeat']:
+            result = list(set(result))
 
         return result
 
@@ -172,6 +225,7 @@ class Scandable(QtWidgets.QWidget):
         self._create_view()
         self._place_components()
         self._create_controller()
+        self._refresh()
 
     def _create_model(self):
         """
@@ -190,6 +244,16 @@ class Scandable(QtWidgets.QWidget):
         self._separators_field = QtWidgets.QLineEdit(self)
         self._per_line_box = QtWidgets.QCheckBox(self)
         self._per_line_box.setText('Analyse one line')
+        self._no_blank_box = QtWidgets.QCheckBox(self)
+        self._no_blank_box.setText('Remove empty strings in result')
+        self._no_repeat_box = QtWidgets.QCheckBox(self)
+        self._no_repeat_box.setText('Remove duplicates in the result')
+
+        self._analyse_button = QtWidgets.QPushButton(self)
+        self._analyse_button.setText('Analyse')
+
+        self._output_field = QtWidgets.QTextEdit(self)
+        self._output_field.setMaximumWidth(400)
 
     def _place_components(self):
         """
@@ -210,13 +274,29 @@ class Scandable(QtWidgets.QWidget):
         opt_layout = QtWidgets.QGridLayout(options_box)
         opt_layout.addWidget(QtWidgets.QLabel(text='Separators : '), 0, 0)
         opt_layout.addWidget(self._separators_field, 0, 1)
-        opt_layout.addWidget(self._per_line_box, 1, 0)
+        opt_layout.addWidget(self._per_line_box, 1, 0, 1, 2)
+        opt_layout.addWidget(self._no_repeat_box, 2, 0, 1, 2)
+        opt_layout.addWidget(self._no_blank_box, 3, 0, 1, 2)
 
         options_box.setLayout(opt_layout)
+
+        output_box = QtWidgets.QGroupBox(self)
+        output_box.setTitle('Output')
+
+        out_layout = QtWidgets.QGridLayout(output_box)
+        out_layout.addWidget(self._output_field, 0, 0)
+
+        output_box.setLayout(out_layout)
 
         main_layout = QtWidgets.QGridLayout(self)
         main_layout.addWidget(input_box, 0, 0)
         main_layout.addWidget(options_box, 1, 0)
+        main_layout.addWidget(self._analyse_button, 2, 0)
+        main_layout.addWidget(output_box, 0, 1, 3, 1)
+
+        main_layout.setRowStretch(0, 0)
+        main_layout.setRowStretch(1, 1)
+        main_layout.setRowStretch(2, 0)
 
         self.setLayout(main_layout)
 
@@ -224,7 +304,75 @@ class Scandable(QtWidgets.QWidget):
         """
         Connect widgets.
         """
-        pass
+        self._file_input_button.clicked.connect(self._open_file)
+        self._analyse_button.clicked.connect(self._run_analysis)
+        self._no_blank_box.clicked.connect(self._on_no_blank_click)
+        self._no_repeat_box.clicked.connect(self._on_no_repeat_click)
+
+    def _refresh(self):
+        """
+        Refresh the view for the first time.
+        """
+        options = self._model.get_options()
+        self._no_repeat_box.setChecked(options['no-repeat'])
+        self._no_blank_box.setChecked(options['no-blank'])
+
+    def _on_no_repeat_click(self):
+        """
+        Called when the user click on checkbox no_repeat_box. Control the 'no-repeat' option in the
+        model.
+        """
+        self._model.set_options({'no-repeat': self._no_repeat_box.isChecked()})
+
+    def _on_no_blank_click(self):
+        """
+        Called when the user click on checkbox no_blank_box. Control the 'no-blank' option in the
+        model.
+        """
+        self._model.set_options({'no-blank': self._no_blank_box.isChecked()})
+
+    def _open_file(self):
+        """
+        Open a popup window asking a file to read. File path is set in file_input_field.
+        """
+        filename, _ = QtWidgets.QFileDialog.getOpenFileName(
+            parent=self,
+            caption='Open file',
+            filter='Text files (*.txt);;All files (*.*)',
+        )
+        self._file_input_field.setText(filename)
+        self._model.set_file(filename)
+
+    def _set_separators(self):
+        """
+        Set separators from separators_field into the model only if not empty.
+        """
+        seps = [sep for sep in self._separators_field.text()]
+        if len(seps) > 0:
+            self._model.set_separators(seps)
+
+    def _run_analysis(self):
+        """
+        Run analysis. Show an error message if something went wrong.
+        """
+        per_line = self._per_line_box.isChecked()
+        self._set_separators()
+
+        try:
+            if per_line:
+                result = self._model.parse_line()
+            else:
+                result = self._model.parse_all()
+        except Exception as e:
+            src.core.widgets.dialog.MessageDialog(
+                text='An error occurs',
+                info='An error occurs and the file has not been parsed. Please see error below.',
+                details='Error : {}'.format(str(e)),
+                icon=QtWidgets.QMessageBox.Critical,
+            ).exec_()
+        else:
+            self._output_field.setText(str(result))
+
 
 loader = src.core.utils.PluginLoader(
     name='Scandable',
